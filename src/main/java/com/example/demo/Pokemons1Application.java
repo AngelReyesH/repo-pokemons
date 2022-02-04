9775a141fea9f1e96cb4c3e2a7c4bd2dcfb850d2
AngelReyesHernandez <angelivan.reyesh@gmail.com>
2022-02-04 07:44:52 -0600

    inicio

--- src/main/java/com/example/demo/Pokemons1Application.java
+++ src/main/java/com/example/demo/Pokemons1Application.java
@@ -2,13 +2,26 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class Pokemons1Application {
+public class Pokemons1Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pokemons1Application.class, args);
 	}
 	
+	
+
+    @RestController
+    public static class WarInitializerController {
+
+        @GetMapping("/")
+        public String handler() {
+           return "Proyecto Iniciado!";
+        }
+    }
 
 }
